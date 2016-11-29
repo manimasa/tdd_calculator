dc99780b23f010e7e17d09826422de67f0f2f026
Martins <martinsjyx90s@yahoo.se>
2016-11-29 19:06:46 +0200

    Refactoring removing if control for empty string and using a default return statement

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -14,9 +14,7 @@ public class Calculator {
     
     public int add(String numbers){
         
-        if(numbers.isEmpty()){
-                return 0;
-        }else if(numbers.length() == 1){
+        if(numbers.length() == 1){
            return Integer.parseInt(numbers);
         }else if(numbers.length() == 2){
             int val_one = Character.getNumericValue(numbers.charAt(0));
