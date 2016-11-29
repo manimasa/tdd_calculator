11bd34b2de69a6e4b23dc19e10aa6446914b9c73
Martins <martinsjyx90s@yahoo.se>
2016-11-29 18:30:36 +0200

    Initialising JUnitCalculator and function testEmptyString

---
+++ test/Test/JUnitCalculator.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Test;
+
+import calculator.Calculator;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+/**
+ *
+ * @author Martins
+ */
+public class JUnitCalculator {
+    private Calculator toTest;
+    public JUnitCalculator() {
+        toTest = new Calculator();
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+     @Test
+     public void testEmptyString() {
+         
+     }
+}
